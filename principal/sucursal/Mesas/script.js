20a830ca156ae5a355fb20a72ee0ccb6df2198c0
Kaaaai2510 <140465805+Kaaaai2510@users.noreply.github.com>
2023-10-21 22:34:00 -0600

    Add files via upload

---
+++ principal/sucursal/Mesas/script.js
@@ -0,0 +1,9 @@
+function ocuparMesa() {
+    const mesaSeleccionada = document.getElementById("mesa").value;
+    const personas = document.getElementById("personas").value;
+
+    // Aquí puedes agregar la lógica para ocupar la mesa si es necesario...
+
+    // Redirigir a la página del menú
+    window.location.href = "Menu";
+}
